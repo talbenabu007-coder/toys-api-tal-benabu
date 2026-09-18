5a003db7b54b33a6c7cb72c0a001fc0176b52603
Tal <talbenabu007@gmail.com>
2026-09-18 19:45:26 +0300

    Add home route

--- routes/configRoutes.js
+++ routes/configRoutes.js
@@ -1,8 +1,10 @@
 const toysR = require("./toys");
 const userR = require("./users");
+const indexR = require("./index")
 
 
 exports.configRoutes = (app) => {
     app.use("/toys", toysR);
     app.use("/users",userR);
+    app.use("/", indexR)
 }
